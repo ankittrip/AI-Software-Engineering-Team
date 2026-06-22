1bd72280089fc34b7d2e9fdd29aad1ebd722db9c
Ankit Tripathi <168020066+ankittrip@users.noreply.github.com>
2026-06-23 02:25:15 +0530

    Update ChromaClient URL to use remote server

--- backend/apps/worker/src/rag/chromaClient.js
+++ backend/apps/worker/src/rag/chromaClient.js
@@ -3,7 +3,7 @@ import { ChromaClient } from "chromadb";
 
 console.log("[DEBUG] CHROMA DB URL IS SET TO:", process.env.CHROMA_URL);
 const client = new ChromaClient({
-  path: process.env.CHROMA_URL || "http://127.0.0.1:8001",
+  path: https: my-chromadb-server.onrender.com || "http://127.0.0.1:8001",
 });
 
 const COLLECTION_NAME = "repo_memory";
